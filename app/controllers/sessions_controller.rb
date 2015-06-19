4bfe430e333b979394b94f512225a3f9d8033388
mcooper <mikeyjc@gmail.com>
2015-06-19 11:39:19 +1000

    basic user login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by :name => params[:name]
+    user = User.find_by :username => params[:username]
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      gon.user = user
-      redirect_to '/app'
+      redirect_to '/'
 
     else
 
@@ -18,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to '/login'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string
+#  password_digest :string
+#  email           :text
+#  admin           :boolean
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class User < ActiveRecord::Base
+  has_secure_password
 end
