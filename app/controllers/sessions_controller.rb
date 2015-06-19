17c2e3cb185cbd66fd528c8e01ab2f305a16ee48
mcooper <mikeyjc@gmail.com>
2015-06-19 14:13:54 +1000

    swapped routes for socketdemo and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by :username => params[:username]
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to :root_path
 
     else
 
       flash[:notice] = "Invalid login, please try again."
-      redirect_to '/'
+      redirect_to :root_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to :root_path
   end
 end
