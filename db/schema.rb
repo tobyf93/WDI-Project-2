c827913cc60fda92cb06d4439a204235ce694543
Toby Flemming <tobyf93@hotmail.com>
2015-06-20 19:11:24 +1000

    Migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619044147) do
+ActiveRecord::Schema.define(version: 20150619055032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "games", force: :cascade do |t|
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.integer  "game_id"
+    t.integer  "user_id"
+    t.integer  "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.boolean  "admin"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -37,4 +47,5 @@ ActiveRecord::Schema.define(version: 20150619044147) do
   end
 
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
