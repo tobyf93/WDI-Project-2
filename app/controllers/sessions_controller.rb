d0f9638d07c681293d9fd2b8bb74a3a0877cb4f7
mcooper <mikeyjc@gmail.com>
2015-06-21 14:58:42 +1000

    sessions are automatically remembered for 20 years without logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,31 @@ class SessionsController < ApplicationController
     user = User.find_by :username => params[:username]
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
+      remember user
       redirect_to :app
     else
       flash[:notice] = "Invalid login, please try again."
       redirect_to :root
+    end
+  end
+
+  # This method is linked to the remember method in the users controller which creates a random hash as a token to remember a user in the app.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # This method determines whether a session is defined by the remember_token and cookie or just by a new user signing in.
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,12 @@
 #  total_wins      :integer          default(0)
 #  created_at      :datetime
 #  updated_at      :datetime
+#  total_games     :integer          default(0)
+#  remember_digest :string
 #
 
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   
   validates :username, :length => { :minimum => 4, :too_short => "must be at least 4 characters long" }, :format => { :with => /\A[a-zA-Z]+\z/, :chars_error => "Only letters allowed." }, :presence => true, :uniqueness => true
   # Username must be at least 4 characters, can only have upper and lower case letters and must be present.
@@ -23,5 +26,27 @@ class User < ActiveRecord::Base
 
   validates :password_digest, :length => { :minimum => 4, :too_short => "must be at least 4 characters long" }, :presence => true
   # Password must be at least 4 characters and must be present.
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  # This method returns the hash digest of the given string so it can be used for the remember token.
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  # This creates the hashed version of the remember token to store it.
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  # This method returns a random string with a length of 22 characters to be used for the remember_me token.
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
 end
