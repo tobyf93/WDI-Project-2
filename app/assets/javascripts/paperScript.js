3ad668d11973f34d8914952bd4a98f4a969415bc
Toby Flemming <tobyf93@hotmail.com>
2015-06-22 14:46:08 +1000

    New path created on every mousedown event

--- app/assets/javascripts/paperScript.js
+++ app/assets/javascripts/paperScript.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
 	channel.bind('draw', function(data) {
 		var drawing = $('#drawing').is(':checked');
 	  console.log(data.x_pos, data.y_pos);
+
+	  path = path || new Path();
 	  addPoint({x: data.x_pos, y: data.y_pos});
 	});
 
@@ -16,6 +18,8 @@ $(document).ready(function(){
 	var path = new Path(); 
 
 	tool.onMouseDown = function(event) {
+		path = new Path();
+
 		var data = {
 	    xPos: event.point.x,
 	    yPos: event.point.y
@@ -39,6 +43,7 @@ $(document).ready(function(){
 	};
 
 	var addPoint = function(point) {
+		console.log(path);
 		path.add(point);
 		console.log('adding point..');
 	};
