a4dfd3a3c9c2bdc729d82532eb354cccb46b6d5f
Toby Flemming <tobyf93@hotmail.com>
2015-06-23 12:29:59 +1000

    Canvas colours and stroke widths working again (client-side only)

--- app/assets/javascripts/paperScript.js
+++ app/assets/javascripts/paperScript.js
@@ -29,7 +29,7 @@ app.canvas = {
 		var channel = this.dispatcher.subscribe('game');
 
 		channel.bind('draw', function(data) {
-		  path = path || new Path();
+		  app.canvas.path = app.canvas.path || new Path();
 		  if (data.new_path) {
 		  	path = new Path();
 		  }
@@ -37,6 +37,7 @@ app.canvas = {
 		  strokeColor = data.stroke_color;
 		  strokeWidth = data.stroke_width;
 
+		  console.log('adding point..');
 		  app.canvas.addPoint({x: data.x_pos, y: data.y_pos});
 		});
 	},
@@ -44,6 +45,8 @@ app.canvas = {
 	setupEvents: function() {
 		this.tool.onMouseDown = this.mouseDownEvent;
 		this.tool.onMouseDrag = this.mouseDragEvent;
+		$('.color').on('click', this.changeColorEvent);
+		$('#strokeWidth').on('input', this.changeStrokeWidthEvent);
 	},
 
 	mouseDownEvent: function(e) {
@@ -75,6 +78,15 @@ app.canvas = {
 		app.canvas.addPoint(e.point);
 	},
 
+	changeColorEvent: function() {
+		var classes = $(this).attr('class').split(' ');
+		app.canvas.strokeColor = classes[1];
+	},
+
+	changeStrokeWidthEvent: function() {
+		app.canvas.strokeWidth = $(this).val();
+	},
+
 	addPoint: function(point) {
 		strokeColor = this.strokeColor || 'black';
 		strokeWidth = this.strokeWidth || 1;
@@ -89,88 +101,5 @@ app.canvas = {
 
 
 
-
-/*
-$(document).ready(function(){
-	if ($('#drawsomeCanv').length === 0) {
-		return;
-	}
-
-	paper.install(window);
-	paper.setup('drawsomeCanv');
-	var tool = new Tool();
-	var path;
-	var strokeColor;
-	var strokeWidth;
-
-	// Socket stuff
-	var dispatcher = new WebSocketRails(window.location.host + '/websocket');
-
-	var channel = dispatcher.subscribe('game');
-
-	channel.bind('draw', function(data) {
-	  path = path || new Path();
-	  if (data.new_path) {
-	  	path = new Path();
-	  }
-
-	  strokeColor = data.stroke_color;
-	  strokeWidth = data.stroke_width;
-
-	  addPoint({x: data.x_pos, y: data.y_pos});
-	});
-
-	tool.onMouseDown = function(event) {
-		path = new Path();
-
-		var data = {
-	    xPos: event.point.x,
-	    yPos: event.point.y,
-	    newPath: true,
-	    strokeColor: strokeColor,
-	    strokeWidth: strokeWidth
-	  };
-
-		dispatcher.trigger('game.draw', data);
-
-		addPoint(event.point);
-	};
-
-	tool.onMouseDrag = function(event) {
-		var data = {
-	    xPos: event.point.x,
-	    yPos: event.point.y,
-	    strokeColor: strokeColor,
-	    strokeWidth: strokeWidth
-	  };
-
-		dispatcher.trigger('game.draw', data);
-
-		addPoint(event.point);
-	};
-
-	var addPoint = function(point) {
-		strokeColor = strokeColor || 'black';
-		strokeWidth = strokeWidth || 1;
-
-		path.strokeColor = strokeColor;
-		path.strokeWidth = strokeWidth;
-		path.add(point);
-		view.draw();	
-	};
-
-	// Color palette
-	$('.color').on('click', function() {
-		var classes = $(this).attr('class').split(' ');
-		strokeColor = classes[1];
-	});
-
-	// Stroke width
-	$('#strokeWidth').on('input', function() {
-		strokeWidth = $(this).val();
-	});
-});
-
-*/
 
 
