621d0895c236c02c1f08d2623bd89b84465e67f4
Toby Flemming <tobyf93@hotmail.com>
2015-06-23 12:10:05 +1000

    Being re-writing canvas code

--- app/assets/javascripts/paperScript.js
+++ app/assets/javascripts/paperScript.js
@@ -1,3 +1,96 @@
+var app = app || {};
+
+$(document).ready(function() {
+	app.canvas.init();
+});
+
+app.canvas = {
+	init: function() {
+		this.setupPaperJS();
+		this.setupDefaults();
+		this.setupSockets();
+		this.setupEvents();
+	},
+
+	setupPaperJS: function() {
+		paper.install(window);
+		paper.setup('drawsomeCanv');
+	},
+
+	setupDefaults: function() {
+		this.tool = new Tool();
+		this.path = undefined;
+		this.strokeColor = 'black';
+		this.strokeWidth = 1;
+	},
+
+	setupSockets: function() {
+		this.dispatcher = new WebSocketRails(window.location.host + '/websocket');
+		var channel = this.dispatcher.subscribe('game');
+
+		channel.bind('draw', function(data) {
+		  path = path || new Path();
+		  if (data.new_path) {
+		  	path = new Path();
+		  }
+
+		  strokeColor = data.stroke_color;
+		  strokeWidth = data.stroke_width;
+
+		  app.canvas.addPoint({x: data.x_pos, y: data.y_pos});
+		});
+	},
+
+	setupEvents: function() {
+		this.tool.onMouseDown = this.mouseDownEvent;
+		this.tool.onMouseDrag = this.mouseDragEvent;
+	},
+
+	mouseDownEvent: function(e) {
+		path = new Path();
+
+		var data = {
+	    xPos: e.point.x,
+	    yPos: e.point.y,
+	    newPath: true,
+	    strokeColor: this.strokeColor,
+	    strokeWidth: this.strokeWidth
+	  };
+
+		app.canvas.dispatcher.trigger('game.draw', data);
+
+		app.canvas.addPoint(e.point);
+	},
+
+	mouseDragEvent: function(e) {
+		var data = {
+	    xPos: e.point.x,
+	    yPos: e.point.y,
+	    strokeColor: this.strokeColor,
+	    strokeWidth: this.strokeWidth
+	  };
+
+		app.canvas.dispatcher.trigger('game.draw', data);
+
+		app.canvas.addPoint(e.point);
+	},
+
+	addPoint: function(point) {
+		strokeColor = this.strokeColor || 'black';
+		strokeWidth = this.strokeWidth || 1;
+
+		path.strokeColor = strokeColor;
+		path.strokeWidth = strokeWidth;
+		path.add(point);
+		view.draw();	
+	}
+
+};
+
+
+
+
+/*
 $(document).ready(function(){
 	if ($('#drawsomeCanv').length === 0) {
 		return;
@@ -78,6 +171,6 @@ $(document).ready(function(){
 	});
 });
 
-
+*/
 
 
